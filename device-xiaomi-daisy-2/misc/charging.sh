bb9a31f420f3dab64c513ef3fbf84dc6062b4ae7
lugo_ <47455213+NotLugozzi@users.noreply.github.com>
2020-11-07 12:09:01 +0100

    Delete charging.sh

--- device-xiaomi-daisy-2/misc/charging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# We'll vibrate a bit here, then turn the led on.
-echo 500 > /sys/class/leds/vibrator/duration                                                                                          
-echo 100 > /sys/class/leds/vibrator/brightness
-echo 1 > /sys/class/leds/vibrator/activate
-echo 150 > /sys/class/leds/red/brightness
